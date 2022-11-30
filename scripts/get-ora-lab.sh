49e4b9901c9147a731d24b61dc31b9d2f9d121d0
Gary Gordhamer <ggordham@gmail.com>
2022-11-30 10:36:17 -0600

    minor change to reboot command

--- scripts/get-ora-lab.sh
+++ scripts/get-ora-lab.sh
@@ -10,6 +10,7 @@ repo_url=https://github.com/ggordham/ora-lab
 package_root=ggordham-ora-lab
 target=scripts
 target_path=/opt/ora-lab
+log_file=/tmp/get-ora-lab.log
 
 trim() {
     local var="$*"
@@ -23,9 +24,9 @@ trim() {
 # Verify tar RPM is installed
 if ! rpm -q --quiet tar; then
     if [ -x /usr/bin/dnf ]; then 
-        /usr/bin/sudo /usr/bin/dnf -y install tar
+        /usr/bin/sudo /usr/bin/dnf -y install tar | /usr/bin/tee -a "${log_file}"
     elif [ -x /usr/bin/yum ]; then
-        /usr/bin/sudo /usr/bin/yum -y install tar
+        /usr/bin/sudo /usr/bin/yum -y install tar | /usr/bin/tee -a "${log_file}"
     else 
         echo "ERROR: TAR not installed and package manager not found"
         exit 1
@@ -37,17 +38,17 @@ fi
 sudo /usr/bin/chown cloud-user:cloud-user "${target_path}"
 
 # download the ora-lab scripts
-/usr/bin/curl -L ${repo_url}/tarball/main | tar xz -C "${target_path}" --strip=1 "${package_root}-???????/${target}"
+/usr/bin/curl -L ${repo_url}/tarball/main | tar xz -C "${target_path}" --strip=1 "${package_root}-???????/${target}"  | /usr/bin/tee -a "${log_file}"
 
 # check if cloud-init is finished then reboot
 while [ ! "$( trim "$( /usr/bin/sudo /usr/bin/cloud-init status | /usr/bin/cut -d: -f2 )" )" == "done" ]; do
-    echo "Waiting for Cloud init to complete, sleeping 30 seconds"
+    echo "Waiting for Cloud init to complete, sleeping 30 seconds" | /usr/bin/tee -a "${log_file}"
     sleep 30
 done
 
 # reboot after cloud-init is finished
 #  Be sure to exit 0 for terraform to get good status
-( /usr/bin/sleep 5 && /usr/bin/sudo /usr/sbin/reboot ) &
+/bin/bash -c "/usr/bin/sleep 10 ; /usr/bin/sudo /usr/sbin/reboot "
 exit 0
 
 # END 
