170b88e27aca9044f7e01c8da2b45c15994c1582
Gary Gordhamer <ggordham@gmail.com>
2023-06-30 08:18:15 -0500

    capturing script errors to log

--- scripts/tstOraInst.sh
+++ scripts/tstOraInst.sh
@@ -33,13 +33,13 @@ ora_home=$( cfgGet "$CONF_FILE" srvr_ora_home )
 # run Linux pre-install items (pre)
 if inListC "${build_steps}" "pre"; then
     logMesg 0 "==== oraLnxPre.sh (pre)" I "${log_file}"
-    /usr/bin/sudo sh -c "${SCRIPTDIR}/oraLnxPre.sh --disks ${disk_list} >> ${log_file}"
+    /usr/bin/sudo sh -c "${SCRIPTDIR}/oraLnxPre.sh --disks ${disk_list} >> ${log_file} 2>&1"
 fi
 
 # download and stage rquired software (stg)
 if inListC "${build_steps}" "stg"; then
     logMesg 0 "==== oraSwStg.sh (stg)" I "${log_file}"
-    /usr/bin/sudo sh -c "${SCRIPTDIR}/oraSwStg.sh --oraver ${ora_ver} --orasubver ${ora_subver} --stgdir ${stg_dir} --orabase ${ora_base} --orahome ${ora_home} >> ${log_file}"
+    /usr/bin/sudo sh -c "${SCRIPTDIR}/oraSwStg.sh --oraver ${ora_ver} --orasubver ${ora_subver} --stgdir ${stg_dir} --orabase ${ora_base} --orahome ${ora_home} >> ${log_file} 2>&1"
 fi
 
 # run software install (inst)
@@ -47,7 +47,7 @@ if inListC "${build_steps}" "inst"; then
     /usr/bin/sudo sh -c "/usr/bin/chmod 666 ${log_file}"
     /usr/bin/sudo sh -c "/usr/bin/chown oracle ${log_file}"
     logMesg 0 "==== oraSwInst.sh (inst)" I "${log_file}"
-    /usr/bin/sudo sh -c "${SCRIPTDIR}/oraSwInst.sh --oraver ${ora_ver} --orasubver ${ora_subver} --stgdir ${stg_dir} --orabase ${ora_base} --orahome ${ora_home} >> ${log_file}"
+    /usr/bin/sudo sh -c "${SCRIPTDIR}/oraSwInst.sh --oraver ${ora_ver} --orasubver ${ora_subver} --stgdir ${stg_dir} --orabase ${ora_base} --orahome ${ora_home} >> ${log_file} 2>&1"
 fi
 
 # run database creation assistant (dbca)
@@ -57,7 +57,7 @@ if inListC "${build_steps}" "dbca"; then
     logMesg 0 "==== oraDBCA.sh (dbca)" I "${log_file}"
     /usr/bin/sudo sh -c "/usr/bin/chmod 774 ${SCRIPTDIR}/oraDBCA.sh"
     /usr/bin/sudo sh -c "/usr/bin/chgrp oinstall ${SCRIPTDIR}/oraDBCA.sh"
-    /usr/bin/sudo -u oracle sh -c "${SCRIPTDIR}/oraDBCA.sh --insecure >> ${log_file}"
+    /usr/bin/sudo -u oracle sh -c "${SCRIPTDIR}/oraDBCA.sh --insecure >> ${log_file} 2>&1"
 fi
 
 # run database creation assistant (lsnr)
@@ -67,7 +67,7 @@ if inListC "${build_steps}" "lsnr"; then
     logMesg 0 "==== oraLsnr.sh (lsnr)" I "${log_file}"
     /usr/bin/sudo sh -c "/usr/bin/chmod 774 ${SCRIPTDIR}/oraLsnr.sh"
     /usr/bin/sudo sh -c "/usr/bin/chgrp oinstall ${SCRIPTDIR}/oraLsnr.sh"
-    /usr/bin/sudo -u oracle sh -c "${SCRIPTDIR}/oraLsnr.sh >> ${log_file}"
+    /usr/bin/sudo -u oracle sh -c "${SCRIPTDIR}/oraLsnr.sh >> ${log_file} 2>&1"
 
     # create TNS entires
     # decide on what SID or PDB to use for install
@@ -78,10 +78,10 @@ if inListC "${build_steps}" "lsnr"; then
     ora_db_sid=$( cfgGet "$CONF_FILE" ora_db_sid )
     ora_db_pdb=$( cfgGet "$CONF_FILE" ora_db_pdb )
     logMesg 0 "==== oraTNS.sh for $ora_db_sid" I "${log_file}"
-    /usr/bin/sudo -u oracle sh -c "${SCRIPTDIR}/oraTNS.sh --dbservice ${ora_db_sid} >> ${log_file}"
+    /usr/bin/sudo -u oracle sh -c "${SCRIPTDIR}/oraTNS.sh --dbservice ${ora_db_sid} >> ${log_file} 2>&1"
     if [ "${ora_db_pdb}" != "__UNDEFINED__" ] || [ -n "${db_db_pdb:-}" ] ; then
         logMesg 0 "==== oraTNS.sh for $ora_db_pdb" I "${log_file}"
-        /usr/bin/sudo -u oracle sh -c "${SCRIPTDIR}/oraTNS.sh --dbservice ${ora_db_pdb} >> ${log_file}"
+        /usr/bin/sudo -u oracle sh -c "${SCRIPTDIR}/oraTNS.sh --dbservice ${ora_db_pdb} >> ${log_file} 2>&1"
     fi 
 fi
 
@@ -92,7 +92,7 @@ logMesg 0 "==== sleep for 60 seconds to allow Listener registration" I "${log_fi
 # Install Oracle Rest Data Services (ords)
 if inListC "${build_steps}" "ords"; then
     logMesg 0 "==== oraORDS.sh (ords)" I "${log_file}"
-    /usr/bin/sudo sh -c "${SCRIPTDIR}/oraORDS.sh >> ${log_file}"
+    /usr/bin/sudo sh -c "${SCRIPTDIR}/oraORDS.sh >> ${log_file} 2>&1"
 fi
 
 # Install Oracle database sample schemas (samp)
@@ -102,13 +102,13 @@ if inListC "${build_steps}" "samp"; then
     logMesg 0 "==== oraDBSamp.sh (samp)" I "${log_file}"
     /usr/bin/sudo sh -c "/usr/bin/chmod 774 ${SCRIPTDIR}/oraDBSamp.sh"
     /usr/bin/sudo sh -c "/usr/bin/chgrp oinstall ${SCRIPTDIR}/oraDBSamp.sh"
-    /usr/bin/sudo -u oracle sh -c "${SCRIPTDIR}/oraDBSamp.sh >> ${log_file}"
+    /usr/bin/sudo -u oracle sh -c "${SCRIPTDIR}/oraDBSamp.sh >> ${log_file} 2>&1"
 fi
 
 # configure oracle user profile (cfg)
 if inListC "${build_steps}" "cfg"; then
     logMesg 0 "==== oraUsrCfg.sh (cfg)" I "${log_file}"
-    /usr/bin/sudo sh -c "${SCRIPTDIR}/oraUsrCfg.sh >> ${log_file}"
+    /usr/bin/sudo sh -c "${SCRIPTDIR}/oraUsrCfg.sh >> ${log_file} 2>&1"
 fi
 
 echo "===============================================================" >> "${log_file}"
